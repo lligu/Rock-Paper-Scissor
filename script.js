01cf1dc27fc40896787ccc6e906632af51483102
lligu <bidone.guglielmo@gmail.com>
2022-03-24 15:19:31 +0100

    Working script

--- script.js
+++ script.js
@@ -1,96 +1,124 @@
-function randomSelection() {
+function restart() {
+    rounds = 0;
+    ties = 0;
+    gameWinner = null;
+    userScore.textContent = 0;
+    computerScore.textContent = 0;
+    winner = null;
+    userPlay = null;
+    computerPlay = null;
+    document.querySelector('#ui h2').style.fontSize = '4rem';
+    document.querySelector('#ui h2').textContent = "Make your move";
+    document.querySelector('#uiMoves').style.height = 'initial';
+    document.querySelector('#uiMoves button').style.display = 'none';
+    userSymbol.style.opacity = '0';
+    computerSymbol.style.opacity = '0';
+    document.querySelectorAll('#uiMoves div').forEach((div) => {
+        div.style.display = 'initial';
+    })
+}
+
+function computerSelection() {
     random = Math.floor(Math.random() * 100) + 1;
     if (random <= 33) {
-        computerSelection = "Rock";
+        computerPlay = "rock";
     } else if (random > 33 && random <= 66) {
-        computerSelection = "Paper";
+        computerPlay = "paper";
     } else {
-        computerSelection = "Scissors";
+        computerPlay = "scissors";
     }
-    return computerSelection;
-} 
+    return computerPlay;
+}
 
-function catchMove() {
-    moveChosen = null;
+function playRound() {
+    userSymbol = document.querySelector('#userMove');
+    computerSymbol = document.querySelector('#computerMove');
+    switch (computerPlay) {
+        case "rock":
+            computerSymbol.style.backgroundPosition = '-20px';
+            if (userPlay === "rock") {
+                userSymbol.style.backgroundPosition = '-20px';
+                winner = 'tie';
+            } else if (userPlay === "paper") {
+                userSymbol.style.backgroundPosition = '-160px';
+                winner = "user";
+            } else if (userPlay === "scissors") {
+                userSymbol.style.backgroundPosition = '-290px';
+                winner = "computer";
+            }
+            break;
+        case "paper":
+            computerSymbol.style.backgroundPosition = '-160px';
+            if (userPlay === "paper") {
+                userSymbol.style.backgroundPosition = '-160px';
+                winner = 'tie';
+            } else if (userPlay === "scissors") {
+                userSymbol.style.backgroundPosition = '-290px';
+                winner = "user";
+            } else if (userPlay === "rock") {
+                userSymbol.style.backgroundPosition = '-20px';
+                winner = "computer";
+            }
+            break;
+        case "scissors":
+            computerSymbol.style.backgroundPosition = '-290px';
+            if (userPlay === "scissors") {
+                userSymbol.style.backgroundPosition = '-290px';
+                winner = 'tie';
+            } else if (userPlay === "rock") {
+                userSymbol.style.backgroundPosition = '-20px';
+                winner = "user";
+            } else if (userPlay === "paper") {
+                userSymbol.style.backgroundPosition = '-160px';
+                winner = "computer";
+            }
+            break;
+    }
+    userSymbol.style.opacity = '1';
+    computerSymbol.style.opacity = '1';
+    return winner;
+}
+
+function playGame() {
+    rounds = 0;
+    ties = 0;
+    gameWinner = null;
     const moves = document.querySelectorAll('.move')
     moves.forEach((move) => {
         move.addEventListener('click', (e) => {
-            moveChosen = e.target.id;
-            console.log(moveChosen);
+            userPlay = e.target.id;
+            computerSelection();
+            playRound();
+            rounds++;
+            userScore = document.querySelector('#userScore');
+            computerScore = document.querySelector('#computerScore');
+            if (winner === 'user') {
+                userScore.textContent = parseInt(userScore.textContent) + 1;
+                if (userScore.textContent === "5") {
+                    gameWinner = 'You';
+                }
+            } else if (winner === 'computer') {
+                computerScore.textContent = parseInt(computerScore.textContent) + 1;
+                if (computerScore.textContent === "5") {
+                    gameWinner = 'The computer';
+                }
+            } else {
+                ties++;
+            }
+            if (gameWinner !== null) {
+                document.querySelector('#ui h2').style.fontSize = '2.5rem';
+                document.querySelector('#ui h2').textContent = gameWinner + ' won the game! There have been ' + rounds + ' rounds, of which ' + ties + ' were ties.';
+                document.querySelector('#uiMoves').style.height = '60%';
+                document.querySelector('#uiMoves button').style.display = 'initial';
+                document.querySelectorAll('#uiMoves div').forEach((div) => {
+                    div.style.display = 'none';
+                })
+                document.querySelector('#uiMoves button').addEventListener('click', () => {
+                    restart();
+                });
+            }
         })
     })
 }
 
-function playRound(computerPlay) {
-    winner = 0;
-    userPlay = prompt("Make your play:", "");
-    while (userPlay.toLowerCase() !== "rock" && userPlay.toLowerCase() !== "paper" && userPlay.toLowerCase() !== "scissors") {
-        userPlay = prompt("Please make a valid play: ", "");
-    }
-    console.log("Your play is " + userPlay);
-    console.log("Computer's play is " + computerPlay);
-    switch (computerPlay) {
-        case "Rock":
-            if (userPlay.toLowerCase() === "rock") {
-                console.log("Tie! " + computerPlay + " ties with " + userPlay);
-            } else if (userPlay.toLowerCase() === "paper") {
-                console.log("Win! " + computerPlay + " loses with " + userPlay);
-                winner = "user";
-            } else if (userPlay.toLowerCase() === "scissors") {
-                console.log("Lose! " + computerPlay + " wins with " + userPlay);
-                winner = "computer";
-            }
-            break;
-        case "Paper":
-            if (userPlay.toLowerCase() === "paper") {
-                console.log("Tie! " + computerPlay + " ties with " + userPlay);
-            } else if (userPlay.toLowerCase() === "scissors") {
-                console.log("Win! " + computerPlay + " loses with " + userPlay);
-                winner = "user";
-            } else if (userPlay.toLowerCase() === "rock") {
-                console.log("Lose! " + computerPlay + " wins with " + userPlay);
-                winner = "computer";
-            }
-            break;
-        case "Scissors":
-            if (userPlay.toLowerCase() === "scissors") {
-                console.log("Tie! " + computerPlay + " ties with " + userPlay);
-            } else if (userPlay.toLowerCase() === "rock") {
-                console.log("Win! " + computerPlay + " loses with " + userPlay);
-                winner = "user";
-            } else if (userPlay.toLowerCase() === "paper") {
-                console.log("Lose! " + computerPlay + " wins with " + userPlay);
-                winner = "computer";
-            }
-            break;
-    }
-    return winner;
-}
-
-function game() {
-    user = 0;
-    computer = 0;
-    rounds = 0;
-    ties = 0;
-    while (user < 3 && computer < 3) {
-        playRound(randomSelection());
-        rounds++;
-        if (winner === "computer") {
-            computer++;
-        } else if (winner === "user") {
-            user++;
-        } else {
-            ties++;
-        }
-    }
-    if (computer === 3) {
-        console.log("The computer wins the game " + computer + "-" + user + ". " + rounds + " were played.");
-    } else if (user === 3) {
-        console.log("You win the game " + user + "-" + computer + ". " + rounds + " were played.");
-    }
-    console.log("There have been " + ties + " ties.")
-}
-
-
-//game();
-catchMove();
+playGame();
